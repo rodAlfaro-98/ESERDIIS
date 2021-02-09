60a4f1bf049cd6f49998ac005acf8822b1fe430c
AlfaroEs <sabua2000@gmail.com>
2021-02-08 20:06:41 -0600

    nueva rama

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,20 @@ function desplegar(elemento,lugar) {
     x.style.display = "block";
   }
   if(elemento.value == 0){
-    x.style.display = "none"
+    x.style.display = "none";
   }
 
 }
+
+function desplegarreporte(elemento) {
+  var x = document.getElementById("opt");
+  var y = document.getElementById("opt1");
+  if(elemento.value == 1){
+    x.style.display = "block";
+    y.style.display = "none";
+  }
+  if(elemento.value == 0){
+    x.style.display = "none";
+    y.style.display = "block";
+  }
+}    
--- views/reportes.php
+++ views/reportes.php
@@ -1,3 +1,56 @@
+<?php
+
+    require ("../libs/Database.php");
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $tipo = '';
+        $genero = '';
+        $menorEdad = '2003-01-01';
+        $comparacionMenor = strtotime($menorEdad);
+        $terceraEdad = '1956-01-01';
+        $conparacionTercera = strtotime($mayorEdad);
+        $consulta = '';
+        $tipo_consulta = '';
+
+        $matriz = array();
+
+        if(isset($_POST["consulttype"])){
+            $tipo = trim($_POST["consulttype"]);
+        }
+        try{
+            $instancia = Database::getInstance();
+            $conexion = $instancia->getConnection();
+            if(tipo == "1"){
+                $tipo_consulta = "demo";
+                $genero = $_POST["genero"];
+                $edad = $_POST["edad"];
+
+            }else if(tipo == "0"){
+                $tipo_consulta = "consulta";
+                $consulta = $_POST["consulta"];
+
+                $sql = "SELECT * FROM usuarios AS u JOIN formulario AS f on u.pkUsuarios = f.fkUsuarios where f.tipo_consulta = ?";
+                $stmt = $conexion->prepare($sql);
+                $stmt->bind_param("s",$consulta);
+                $stmt->execute();
+                $usuarios = $stmt->get_result();
+
+                while($row = $usuario -> fetch_assoc()){
+                    $resultado = array();
+                    foreach($row as $cname => $cvalue){
+                        //print "$cname: $cvalue\t";
+                        $resultado[$cname] = $cvalue;
+                    }
+                    array_push($matriz,$resultado);
+                }
+
+            }
+        }catch(Exception $err){
+            echo $err;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -12,7 +65,7 @@
         <?php
             include ("navigation.php");
         ?>
-
+ 
         <div id="page-wrapper" class="gray-bg">
         <div class="row border-bottom">
 
@@ -22,110 +75,117 @@
                 <h2>Registro</h2>
                 </div>
                 
-                <table>
-                    <tr>
-                        <td>
-                            <div class="form-check">
-                                <input class="form-check-input radium-b" type="radio" name="fiebre" id="fiebresi" value="1" onclick="desplegar(this,'place')" required>
-                                <label class="form-check-label"  for="fiebresi">
-                                Genero
-                                </label>
-                            <div class:"row">    
-                                <div id="place" style="display: none;">
-                                    <table>
-                                        <tr>
-                                            <th>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="genero" id="hombre" value="H">
-                                                    <label class="form-check-label" for="hombre">
-                                                        Hombre
-                                                    </label>
-                                                </div>
-                                            </th>
-                                            <th>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="genero" id="mujer" value="M">
-                                                    <label class="form-check-label" for="mujer">
-                                                        Mujer
-                                                    </label>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="genero" id="otro" value="O">
-                                                    <label class="form-check-label" for="otro">
-                                                        Otro
-                                                    </label>
-                                                </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="edad" id="edad1" value="0">
-                                                    <label class="form-check-label" for="edad1">
-                                                        menor a 18 años
-                                                    </label>
-                                                </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="edad" id="edad2" value="1">
-                                                    <label class="form-check-label" for="edad2">
-                                                        adultos
-                                                    </label>
-                                                </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="edad" id="edad3" value="2">
-                                                    <label class="form-check-label" for="edad3">
-                                                        tercera edad
-                                                    </label>
-                                                </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </table>
+                <form class="m-t" role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                    <table width="100%" style="margin: 0px;">
+                        <tr>
+                            <td>
+                                <div class="form-check">
+                                    <input class="form-check-input radium-b" type="radio" name="consulttype" value="1" onclick="desplegarreporte(this)" required>
+                                    <label class="form-check-label"  for="fiebresi">
+                                    Genero
+                                    </label>
+                                <div class:"row">    
+                                    <div id="opt" style="display: none;">
+                                        <table>
+                                            <tr>
+                                                <th>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="genero" id="hombre" value="H">
+                                                        <label class="form-check-label" for="hombre">
+                                                            Hombre
+                                                        </label>
+                                                    </div>
+                                                </th>
+                                                <th>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="genero" id="mujer" value="M">
+                                                        <label class="form-check-label" for="mujer">
+                                                            Mujer
+                                                        </label>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="genero" id="otro" value="O">
+                                                        <label class="form-check-label" for="otro">
+                                                            Otro
+                                                        </label>
+                                                    </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="edad" id="edad1" value="0">
+                                                        <label class="form-check-label" for="edad1">
+                                                            Menor a 18 años
+                                                        </label>
+                                                    </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="edad" id="edad2" value="1">
+                                                        <label class="form-check-label" for="edad2">
+                                                            Adultos
+                                                        </label>
+                                                    </div>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="edad" id="edad3" value="2">
+                                                        <label class="form-check-label" for="edad3">
+                                                            Tercera edad
+                                                        </label>
+                                                    </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="form-check">
-                                <input class="form-check-input radium-b" type="radio" name="fiebre" id="fiebresi" value="1" onclick="desplegar(this,'place1')" required>
-                                <label class="form-check-label"  for="fiebresi">
-                                Consulta
-                                </label>
-                            <div class:"row">    
-                                <div id="place1" style="display: none;">
-                                    <table>
-                                        <tr>
-                                            <th>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="consulta" id="urgencias" value="urgencias">
-                                                    <label class="form-check-label" for="urgencias">
-                                                        Urgencias
-                                                    </label>
-                                                </div>
-                                            </th>
-                                            <th>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="consulta" id="externa" value="consulta externa">
-                                                    <label class="form-check-label" for="consulta externa">
-                                                        Consulta Externa
-                                                    </label>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </table>
+                            </td>
+                            <td>
+                                <div class="form-check">
+                                    <input class="form-check-input radium-b" type="radio"  name="consulttype" value="0" onclick="desplegarreporte(this)" required>
+                                    <label class="form-check-label"  for="fiebresi">
+                                    Consulta
+                                    </label>
+                                <div class:"row">   
+                                    <div id="opt1" style="display: none;">
+                                        <table>
+                                            <tr>
+                                                <th>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="consulta" id="urgencias" value="urgencias">
+                                                        <label class="form-check-label" for="urgencias">
+                                                            Urgencias
+                                                        </label>
+                                                    </div>
+                                                </th>
+                                                <th>
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="consulta" id="externa" value="consulta externa">
+                                                        <label class="form-check-label" for="consulta externa">
+                                                            Consulta Externa
+                                                        </label>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </div>
                                 </div>
+                            </td>
+                            <td>
+                            <div id="bton" style="display: none;">
+                                <button type="submit" class="btn btn-primary block full-width m-b">Buscar</button>
                             </div>
-                        </td>
-                    </tr>
-                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
             
         <div class="wrapper wrapper-content animated fadeInRight">
             <div class="row">
@@ -149,8 +209,17 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php
-
+                        <?php   
+                            if(!empty($matriz))
+                            {
+                                foreach($matriz as $fila){
+                                    echo "<tr>";
+                                    foreach($fila as $value){
+                                        echo "<td>".$value."</td>";
+                                    }
+                                    echo "</tr>";
+                                }
+                            }
                         ?>
                     </tbody>
                     <tfoot>
@@ -164,11 +233,8 @@
             </div>
         </div>
         <div class="footer">
-            <div class="float-right">
-                10GB of <strong>250GB</strong> Free.
-            </div>
             <div>
-                <strong>Copyright</strong> Example Company &copy; 2014-2018
+                <strong>Copyright</strong> ESERDI &copy; 2021                            
             </div>
         </div>
 
